4d25466225ad4055d010734d903ae1e4d9e96682
daiiz <da.iz.mg@gmail.com>
2015-09-27 22:38:51 +0900

    お気に入りスタイル

---
+++ Polymer/scrollHeader/main.js
@@ -0,0 +1,15 @@
+// custom transformation: scale header's title
+var title = document.querySelector('.title');
+addEventListener('paper-header-transform', function(e) {
+    var d = e.detail;
+    var m = d.height - d.condensedHeight;
+    var scale = Math.max(0.75, (m - d.y) / (m / 0.25)  + 0.75);
+    Polymer.Base.transform('scale(' + scale + ') translateZ(0)', title);
+});
+
+window.addEventListener('load', function () {
+    var contents = document.querySelector('#contents');
+    for (var i = 0; i < 100; i++) {
+        contents.innerHTML += '<p style="margin: 15px;">Contents '+ i +'</p>';
+    }
+}, false);
