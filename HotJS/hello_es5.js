4b395beb3052899bae30af7e06724d58c03018aa
daiiz <da.iz.mg@gmail.com>
2015-10-04 00:19:20 +0900

    hello ES6 and babel!

---
+++ HotJS/hello_es5.js
@@ -0,0 +1,12 @@
+// ES6!!!!!
+
+'use strict';
+
+var hello = function hello() {
+  return 'Hello, ES6 world!';
+};
+
+// これを
+// $ babel hello_es6.js > hello_es5.js
+// トランスパイルすると、hello_es5.jsにES5が生成されて最高
+
---
+++ HotJS/hello_es6.js
@@ -0,0 +1,7 @@
+// ES6!!!!!
+
+var hello = () => 'Hello, ES6 world!';
+
+// これを
+// $ babel hello_es6.js > hello_es5.js
+// トランスパイルすると、hello_es5.jsにES5が生成されて最高
