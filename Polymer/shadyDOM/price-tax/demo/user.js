6667059373f7045cad5adae90607d09242941f1a
daiiz <da.iz.mg@gmail.com>
2015-10-04 16:21:13 +0900

    時間かかってしまった

--- Polymer/shadyDOM/price-tax/demo/user.js
+++ Polymer/shadyDOM/price-tax/demo/user.js
@@ -1,17 +1,23 @@
-// Polymer & ES6 (& JSX) !!!!
+// Polymer & ES6 !!!!
 // $ babel user-custom.js > user.js
 
 // よろしくないけど仕方ない
+'use strict';
+
 var React = {};
-React.createElement = (...args) => {
+React.createElement = function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+    }
+
     var name = args[0];
     var props = args[1];
     if (name === null) return;
     var parent = document.createElement(name);
-    args.forEach((arg, i) => {
+    args.forEach(function (arg, i) {
         if (i >= 2) {
-            var child = arg;    // 再帰呼び出し
-            if (typeof(child) == 'string') {
+            var child = arg;
+            if (typeof child == 'string') {
                 Polymer.dom(parent).appendChild(document.createTextNode(child));
             } else {
                 Polymer.dom(parent).appendChild(child);
@@ -20,36 +26,39 @@ React.createElement = (...args) => {
     });
     if (props !== null) {
         var keys = Object.keys(props);
-        keys.forEach((key) => {
+        keys.forEach(function (key) {
             parent.setAttribute(key, props[key]);
         });
     }
     return parent;
 };
 
-window.addEventListener('WebComponentsReady', (e) => {
+window.addEventListener('WebComponentsReady', function (e) {
 
     // カルビ盛合せ
-    let calvi = document.querySelectorAll('price-tax')[0];
+    var calvi = document.querySelectorAll('price-tax')[0];
 
     // 「見たままDOM」の操作
-    let priceTaxElement = calvi;
+    var priceTaxElement = calvi;
     console.info('Light DOM:');
     console.log(priceTaxElement.innerHTML);
     console.dirxml(priceTaxElement.children);
 
+    var salad = React.createElement(
+        'price-tax',
+        { price: '400', rate: '8', switchable: true },
+        '山盛りサラダ'
+    );
+    document.querySelector('body').appendChild(salad);
+    console.log(Polymer.dom(salad).innerHTML);
+
     // 「Shady DOM」の世界を覗く
-    let shadyWorld = Polymer.dom(document.body).children.push();
+    var shadyWorld = Polymer.dom(document.body).children.push();
 
     // 「Shady DOM」の操作
     priceTaxElement = Polymer.dom(calvi);
     console.info('Shady DOM:');
     console.log(priceTaxElement.innerHTML);
     console.dirxml(priceTaxElement.children);
-
-
-    // 山盛りサラダを追加（Syntax Sugar）
-    let salad = <price-tax price="400" rate="8" switchable>山盛りサラダ</price-tax>;
-    document.querySelector('body').appendChild(salad);
-    console.log(Polymer.dom(salad).innerHTML);
 });
+
